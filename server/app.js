e6b98dfce070d740d86a20f99caba7397cdd795f
OsamaBangash8840 <osama.khan8840@gmail.com>
2024-07-16 13:22:26 -0700

    git ignore added

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const corsOption = {
     exposedHeaders :  ['Content-Length','Authorization','token'],
-    origin:'http://localhost:3000',
+    origin:'https://cs-dept-donation-site.netlify.app',
     methods:['GET','POST','PUT','DELETE'],
     credentials:true
   }
